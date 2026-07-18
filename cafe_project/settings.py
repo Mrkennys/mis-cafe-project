60f422c0be0e95a5fc140204b34cad4d53d2849f
Mrkennys <kenish.shrestha01@gmail.com>
2026-07-18 11:38:57 +0545

    Allow network hosts

--- cafe_project/settings.py
+++ cafe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6zcq9yd59yt@iq&7g@jdgx&p8%ud02_a_3-3)wnwhgmbxvi18#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
